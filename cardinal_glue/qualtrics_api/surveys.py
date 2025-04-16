4ef638ea6e77773ed2cce18a203c29d825f6a1f7
Bryce Grier <bdgrier@stanford.edu>
2025-04-15 17:23:43 -0700

    updated retry function

--- cardinal_glue/qualtrics_api/surveys.py
+++ cardinal_glue/qualtrics_api/surveys.py
@@ -105,15 +105,15 @@ class Survey():
             post_response = requests.request('POST', url_post, headers=headers, data=json.dumps(publish_data))
         elif put_response.status_code == 500:
             max_retries = 5
-            retry_count = 1
+            retry_count = 0
             while put_response.status_code == 500:
                 put_response = requests.request('PUT', url_put, headers=headers, data=question_data_json)
                 retry_count += 1
                 if retry_count > max_retries:
-                    print(f"Exceeded maximum retries. Unable to update question {question_ID} : {put_response}")
+                    print(f"Exceeded maximum retries. Unable to update question {question_ID} : {put_response.json}")
                     return
                 sleep_interval = 2 ** retry_count
-                print(f"Checking again in {sleep_interval} seconds")
+                print(f"Trying update question again in {sleep_interval} seconds")
                 time.sleep(sleep_interval) 
         else:
             print(f'Unable to update question {question_ID} : {put_response}')
