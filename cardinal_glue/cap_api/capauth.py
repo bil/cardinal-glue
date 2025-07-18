ca279e2e03b52109d0a613445505670be26de314
Bryce Grier <bdgrier@stanford.edu>
2025-07-18 14:42:27 -0700

    Edit capauth.py

--- cardinal_glue/cap_api/capauth.py
+++ cardinal_glue/cap_api/capauth.py
@@ -29,11 +29,12 @@ class CAPAuth(Auth):
         if auto_auth:
             self.authenticate()
 
-    def authenticate(self):
+    def authenticate(self, file_path=None):
         """
         Attempt to authenticate with the Stanford CAP API.
         """
-        file_path = os.path.join(self._AUTH_PATH, self.__CAP_AUTH_JSON_NAME)
+        if not file_path:
+            file_path = os.path.join(self._AUTH_PATH, self.__CAP_AUTH_JSON_NAME)
         if os.path.exists(file_path):
             f = open(file_path)
             user_info = json.load(f)
